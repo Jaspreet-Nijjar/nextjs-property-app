c8f73903564c7d906da9f9281e9f98b28147a85c
Jaspreet <jaspreetkn92@gmail.com>
2024-12-30 10:35:09 +0000

    added router and params hook to property page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+
+import { useRouter, useParams } from 'next/navigation';
+
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  const { id } = useParams(); //params.id
+
+  return (
+    <div>
+      <h1>Property Page</h1>
+      <p>{id}</p>
+    </div>
+  );
 };
 
 export default PropertyPage;
