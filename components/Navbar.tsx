67f7e5e3c60f35d26e6d24590a4a6c317c733f40
Jaspreet <jaspreetkn92@gmail.com>
2025-01-01 10:24:06 +0000

    added conditional rendering to mobile menu login button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -226,10 +226,11 @@ const Navbar = () => {
               </Link>
             )}
 
-            <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-              <FaGoogle className="text-white mr-2" />
-              <span>Login or Register</span>
-            </button>
+            {!isLoggedIn && (
+              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
+                <span>Login or Register</span>
+              </button>
+            )}
           </div>
         </div>
       )}
