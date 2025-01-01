fa802ecaac250b232859669a1cc3488e05bb765f
Jaspreet <jaspreetkn92@gmail.com>
2025-01-01 10:12:47 +0000

    conditionally rendered pages using pathname hook

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -69,13 +69,17 @@ const Navbar = () => {
                 </Link>
                 <Link
                   href="/properties"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={`${
+                    pathname === '/properties' ? 'bg-black' : ''
+                  }, text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                 >
                   Properties
                 </Link>
                 <Link
-                  href="/add-property"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  href="/properties/add"
+                  className={`${
+                    pathname === '/properties/add' ? 'bg-black' : ''
+                  }, text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                 >
                   Add Property
                 </Link>
