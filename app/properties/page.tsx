f5ad35090617939fa02740bce2947129a4b377df
Jaspreet <jaspreetkn92@gmail.com>
2025-01-03 10:34:00 +0000

    added properties to the Property Page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,5 +1,20 @@
+import properties from '../../properties.json';
+
 const PropertiesPage = () => {
-  return <div>PropertiesPage</div>;
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6"></div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          properties.map((property) => (
+            <div key={property._id}>{property.name}</div>
+          ))
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;
