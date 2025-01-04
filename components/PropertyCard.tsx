fa4f011c30374816a3e574a2d79a2e2ea1078fca
Jaspreet <jaspreetkn92@gmail.com>
2025-01-04 11:31:28 +0000

    revised Link

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -75,14 +75,17 @@ const PropertyCard = ({ property }) => {
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="flex align-middle gap-2 mb-4 lg:mb-0">
             <i className="fa-solid fa-location-dot text-lg text-orange-700"></i>
-            <span className="text-orange-700"> Boston MA </span>
+            <span className="text-orange-700">
+              {' '}
+              {property.location.city} {property.location.state}
+            </span>
           </div>
-          <a
-            href="property.html"
+          <Link
+            href={`/properties/${property.id}`}
             className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
