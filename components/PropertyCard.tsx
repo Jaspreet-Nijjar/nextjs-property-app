da1877265558d5f0db6c97938bc13f20023d8768
Jaspreet <jaspreetkn92@gmail.com>
2025-01-04 11:24:57 +0000

    created a getRateDisplay function

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -2,6 +2,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const PropertyCard = ({ property }) => {
+  const getRateDisplay = () => {
+    const { rates } = property;
+
+    if (rates.monthly) {
+      return `${rates.monthly.toLocaleString()}/month`;
+    } else if (rates.weekly) {
+      return `${rates.weekly.toLocaleString()}/weekly`;
+    } else if (rates.nightly) {
+      return `${rates.nightly.toLocaleString()}/night`;
+    }
+  };
+
   return (
     <div className="rounded-xl shadow-md relative">
       <Image
@@ -15,8 +27,8 @@ const PropertyCard = ({ property }) => {
 
       <div className="p-4">
         <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600">Apartment</div>
-          <h3 className="text-xl font-bold">Boston Commons Retreat</h3>
+          <div className="text-gray-600">{property.type}</div>
+          <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
         <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
           $4,200/mo
