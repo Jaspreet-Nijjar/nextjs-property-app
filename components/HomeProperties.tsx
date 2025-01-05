0599143063a67f3c8c1d3d8995fadfcd446de4a3
Jaspreet <jaspreetkn92@gmail.com>
2025-01-05 09:42:29 +0000

    creating HomeProperties component

---
+++ components/HomeProperties.tsx
@@ -0,0 +1,15 @@
+import properties from '../properties.json';
+
+const HomeProperties = () => {
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+          Recent Properties
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default HomeProperties;
