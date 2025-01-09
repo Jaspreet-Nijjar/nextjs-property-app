d74eae20900f46f2c035c39b4e31b126f6a2d7a7
Jaspreet <jaspreetkn92@gmail.com>
2025-01-09 07:29:04 +0000

    added fetchProperties function to the Properties page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,18 +1,7 @@
 import PropertyCard from '@/components/PropertyCard';
+import { fetchProperties } from '@/utils/requests';
 
-async function fetchProperties() {
-  try {
-    const res = await fetch('http://localhost:3000/api/properties');
-
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+fetchProperties();
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
