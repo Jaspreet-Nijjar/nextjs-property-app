8348bc1b4673f42fd208381aef78e2090c7607f2
Jaspreet <jaspreetkn92@gmail.com>
2025-01-10 09:55:12 +0000

    added content to PropertyHeaderImage

--- components/PropertyHeaderImage.tsx
+++ components/PropertyHeaderImage.tsx
@@ -1,5 +1,18 @@
 const PropertyHeaderImage = () => {
-  return <div>PropertyHeaderImage</div>;
+  return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <img
+            src="images/properties/a1.jpg"
+            alt=""
+            className="object-cover h-[400px] w-full"
+            width="1800"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PropertyHeaderImage;
