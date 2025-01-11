a6a0ac669cdf149194307df2ab1c300ba9330d81
Jaspreet <jaspreetkn92@gmail.com>
2025-01-11 11:32:36 +0000

    conditionally rendering rates

--- components/PropertyDetails.tsx
+++ components/PropertyDetails.tsx
@@ -11,11 +11,14 @@ const PropertyDetails = ({ property }) => {
   return (
     <main>
       <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-        <div className="text-gray-500 mb-4">Apartment</div>
-        <h1 className="text-3xl font-bold mb-4">Boston Commons Retreat</h1>
+        <div className="text-gray-500 mb-4">{property.type}</div>
+        <h1 className="text-3xl font-bold mb-4">{property.name}</h1>
         <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
           <FaMapMarker className="text-lg text-orange-700 mr-2" />
-          <p className="text-orange-700">120 Tremont Street Boston, MA 02111</p>
+          <p className="text-orange-700">
+            {property.location.street} {property.location.city}{' '}
+            {property.location.state} {property.location.zipcode}
+          </p>
         </div>
 
         <h3 className="text-lg font-bold my-6 bg-gray-800 text-white p-2">
@@ -24,17 +27,33 @@ const PropertyDetails = ({ property }) => {
         <div className="flex flex-col md:flex-row justify-around">
           <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
             <div className="text-gray-500 mr-2 font-bold">Nightly</div>
-            <div className="text-2xl font-bold">
-              <FaTimes className="text-red-700" />
+            <div className="text-2xl font-bold text-blue-500">
+              {property.rates.nightly ? (
+                `$${property.rates.nightly.toLocaleString()}`
+              ) : (
+                <FaTimes className="text-red-700" />
+              )}
             </div>
           </div>
           <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
             <div className="text-gray-500 mr-2 font-bold">Weekly</div>
-            <div className="text-2xl font-bold text-blue-500">$1,100</div>
+            <div className="text-2xl font-bold text-blue-500">
+              {property.rates.weekly ? (
+                `$${property.rates.weekly.toLocaleString()}`
+              ) : (
+                <FaTimes className="text-red-700" />
+              )}
+            </div>
           </div>
           <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
             <div className="text-gray-500 mr-2 font-bold">Monthly</div>
-            <div className="text-2xl font-bold text-blue-500">$4,200</div>
+            <div className="text-2xl font-bold text-blue-500">
+              {property.rates.monthly ? (
+                `$${property.rates.monthly.toLocaleString()}`
+              ) : (
+                <FaTimes className="text-red-700" />
+              )}
+            </div>
           </div>
         </div>
       </div>
